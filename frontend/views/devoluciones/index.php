e5a217b20f4f068ed760c5929248e3fd7e2d6fc3
Oscar I. Rojas Hernandez <oscar.rojas991@gmail.com>
2020-03-12 19:02:10 -0600

    Se escondio campo de observaciones en index de 'Devoluciones'

--- frontend/views/devoluciones/index.php
+++ frontend/views/devoluciones/index.php
@@ -30,7 +30,7 @@ $this->params['breadcrumbs'][] = $this->title;
             'alumnoNoControl',
             'alumnoNombre',
             'recibeNombre',
-            'observaciones:ntext',
+            //'observaciones:ntext',
 
             ['class' => 'yii\grid\ActionColumn'],
         ],
